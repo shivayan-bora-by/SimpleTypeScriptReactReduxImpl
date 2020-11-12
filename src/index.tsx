ef857e0750cbb6b201acfcef0f7cd9ecd7da1338
Shivayan Bora 1022042 <shivayananand.bora@blueyonder.com>
2020-11-12 16:19:10 +0530

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,34 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import { App } from './App';
+import { UserList } from './UserList';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+            <Route
+                exact
+                path='/'
+                render={(props) => <App userType='admin' username='h4x0r' {...props} />}
+            />
+        </Switch>
+        <Switch>
+            <Route
+                exact
+                path='/userlist'
+                render={(props) => <UserList {...props} />}
+            />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
